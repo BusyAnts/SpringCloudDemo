75bd0dfb77175029fc419f3ea030a8003cd9d7ce
BusyAnts <chenz16888@163.com>
2019-08-27 09:10:07 +0800

    服务发现

--- microservice-springcloud-parent/microservice-service-order/src/main/java/com/cimc/cloud/controller/OrderController.java
+++ microservice-springcloud-parent/microservice-service-order/src/main/java/com/cimc/cloud/controller/OrderController.java
@@ -1,10 +1,15 @@
 package com.cimc.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author chenz
@@ -17,15 +22,39 @@ public class OrderController {
     @Autowired
     private RestTemplate restTemplate;
 
-    /*
-    @RequestMapping("/orderToMember/{id}")
-    public String orderToMember(@PathVariable Long id) {
+
+    /**
+     * 原始调用方式
+     * @param id
+     * @return
+     */
+    @RequestMapping("/orderToMemberOri/{id}")
+    public String orderToMemberOri(@PathVariable Long id) {
         return restTemplate.getForObject("http://localhost:7901/member/getMember/" + id, String.class);
-    }*/
+    }
 
     @RequestMapping("/orderToMember/{id}")
     public String orderToMember(@PathVariable Long id) {
         return restTemplate.getForObject("http://microservice-service-member/member/getMember/" + id, String.class);
     }
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/discovery", method = RequestMethod.GET)
+    public List<ServiceInstance> discovery() {
+        // 获取所有服务名
+        List<String> list = client.getServices();
+        System.out.println("**********" + list);
+
+        // 获取microservice-service-member服务信息
+        List<ServiceInstance> srvList = client.getInstances("microservice-service-member");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return srvList;
+    }
+
 
 }
