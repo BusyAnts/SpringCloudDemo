e86160ca6c3edaf059c2a696715818fb19c82ce9
BusyAnts <chenz16888@163.com>
2019-08-29 13:57:49 +0800

    通用方式使用Hystrix监控

--- microservice-springcloud-parent/microservice-service-order-common-hystrix/src/main/java/com/cimc/cloud/OrderCommonHystrixApplication.java
+++ microservice-springcloud-parent/microservice-service-order-common-hystrix/src/main/java/com/cimc/cloud/OrderCommonHystrixApplication.java
@@ -2,18 +2,16 @@ package com.cimc.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author chenz
- * @create 2019-08-26 13:19
+ * @create 2019-08-29 13:40
  */
 @SpringBootApplication
 @EnableEurekaClient
-@EnableCircuitBreaker
-public class OrderHystrixCommonApplication {
+public class OrderCommonHystrixApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OrderHystrixCommonApplication.class, args);
+        SpringApplication.run(OrderCommonHystrixApplication.class, args);
     }
 }
