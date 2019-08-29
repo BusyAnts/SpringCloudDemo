39c70d781b001b2d2e4dc2ca05259d7bda37a168
BusyAnts <chenz16888@163.com>
2019-08-29 14:20:17 +0800

    Feign Client使用Hystrix监控

--- microservice-springcloud-parent/microservice-service-order-feign-hystrix/src/main/java/com/cimc/cloud/OrderFeignHystrixApplication.java
+++ microservice-springcloud-parent/microservice-service-order-feign-hystrix/src/main/java/com/cimc/cloud/OrderFeignHystrixApplication.java
@@ -2,6 +2,7 @@ package com.cimc.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -12,6 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class OrderFeignHystrixApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderFeignHystrixApplication.class, args);
