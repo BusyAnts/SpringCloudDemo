ae3f0355fc040a123417caf6b826441933ff2d0c
BusyAnts <chenz16888@163.com>
2019-09-03 13:51:22 +0800

    Spring Cloud Config手动刷新配置

--- microservice-springcloud-parent/microservice-config-client/src/main/java/com/cimc/cloud/controller/IndexController.java
+++ microservice-springcloud-parent/microservice-config-client/src/main/java/com/cimc/cloud/controller/IndexController.java
@@ -1,10 +1,16 @@
 package com.cimc.cloud.controller;
 
+import com.cimc.cloud.model.ConfigData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author chenz
+ */
 @RestController
+//@RefreshScope
 public class IndexController {
     @Value("${version}")
     private String version;
@@ -13,4 +19,13 @@ public class IndexController {
     private String getVersion() {
         return version;
     }
+
+    @Autowired
+    private ConfigData configData;
+
+    @RequestMapping("/getVersion2")
+    private String getVersion2() {
+        return configData.getVersion();
+    }
+
 }
